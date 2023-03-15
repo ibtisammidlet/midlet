177bd674f04e18b9cade4ba86991228994a5eadc
ibtisam midlet <storebitid@gmail.com>
2023-03-15 03:22:13 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-var bulk = require(‘bulk-require’); 
-var files = bulk(__dirname, ‘*.js’);
+var bulk = require("bulk-require"); 
+var files = bulk(__dirname, "*.js");
 
-module.exports = files;
+// now i need code that gives concat the modules object
+var concat = require("concat"); 
+// how from here

09ace547465246d258db9057d56ab04afd7b561f
ibtisam midlet <storebitid@gmail.com>
2023-04-08 01:36:43 +0100

    *

--- short_functions.js
+++ short_functions.js
@@ -1,2 +1,6 @@
 //* on navigation , check url, do, otherwise
 window.onbeforeunload = const checkURL = () => window.location.href.includes("https://discord.com/login") ? /* Do something */ $(".appMount-2yBXZl", "body").css("background", "var(--bg-overlay-app-frame,url(https://i.imgur.com/PKRXF8e.jpeg))") : /* Do something else */ $(".appMount-2yBXZl", "body").css("background", "var(--bg-overlay-app-frame,var(--background-tertiary))")  ;
+
+
+// this will express date where it placed
+var date = new Date(); console.log(date.getFullYear()+'-'+(date.getMonth()+1)+'-'+date.getDate());
